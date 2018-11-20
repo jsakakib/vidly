dc5dd4d800125c49e883b64a8b81b042adf17d71
Jaime Sakakibara <jsakakib@gmail.com>
2018-11-20 09:35:44 -0200

    Exception handling implementation

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
+winston.handleExceptions(
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+
+process.on('unhandledRejection', (ex) => {
+    throw ex;
+});
+
 winston.add(winston.transports.File, { filename: 'logfile.log' });
+
+// throw new Error('Something failed during startup');
 
 if (!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined.');
